e7bd600b6fd73002c8e2b1b9cf918ff7c104e6d3
Kirill <43477428+kirilpetrov36@users.noreply.github.com>
2022-08-09 20:38:11 +0300

    First models created

---
+++ Blog/DAL/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using Blog.DAL.Models.Interfaces;
+
+namespace Blog.DAL.Models
+{
+    public class Image : IDate
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+        public string AltUrl { get; set; }
+        public Post PostRef { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
---
+++ Blog/DAL/Models/Like.cs
@@ -0,0 +1,24 @@
+﻿using Blog.DAL.Models.Interfaces;
+
+namespace Blog.DAL.Models
+{
+    public class PostLike : ILike
+    {
+        public long Id { get; set; }
+        public User User { get; set; }
+        public Post Post { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool isEnable { get; set; }
+    }
+
+    public class CommentLike : ILike
+    {
+        public long Id { get; set; }
+        public User User { get; set; }
+        public Comment Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool isEnable { get; set; }
+    }
+}
